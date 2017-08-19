a70b9b5da46891f5bf0b7730ae8777e7017d50de
Joabian Alvarez <joabian.alvarez@ge.com>
2017-08-18 19:01:31 -0600

    more views

--- Controllers/EquiposViewController.cs
+++ Controllers/EquiposViewController.cs
@@ -14,5 +14,12 @@ namespace simeAlcatraz.Controllers
         {
             return View();
         }
+
+        public ActionResult verInventario()
+        {
+            return View();
+        }
+
+
 	}
 }
