2af0d7a05a649204e8a4545601a74787c39e57e8
Joabian Alvarez <joabian.alvarez@ge.com>
2017-09-05 18:23:40 -0600

    commit sin gitignore

--- Scripts/APIScripts/Controllers/EquipoController.js
+++ Scripts/APIScripts/Controllers/EquipoController.js
@@ -381,43 +381,7 @@
     $scope.loadActioncarEsp = function () {
         $scope.valAction = $scope.seloadAction;
     }
-    $scope.udtEquipo = function () {
-        var isSerializado = $scope.checked;
-        var serial = "N/A";
-        if (isSerializado != undefined) {
-            if (isSerializado) { if ($scope.noserie != undefined) { serial = $scope.noserie; } else { isSerializado = false; } }
-        }
-
-        else { isSerializado = false; }
-
-        var equipo = {
-            equipoID:$scope.idequipo,
-            descripcion: $scope.descripcion,
-            fechaIngreso: new Date(),
-            id_categoria: 1,
-            id_subcategoria: 1,
-            activo: true,
-            marcaEquipo: $scope.marcaEquipo,
-            nombreEquipo: $scope.nombreEquipo,
-            modeloEquipo: $scope.modelo,
-            serializado: isSerializado,
-            numeroSerie: serial,
-            fechaModifico: new Date(),
-            userModifico: 'admin',
-            userCaptura: 'admin',
-            areaStatus: false,
-            hasCheckList: false,
-            periodoServ: false,
-            periodoServNum: 0
-        };
-        console.log(equipo);
-        var saveEquipo = Services.updateEquipo(equipo);
-        saveEquipo.then(function (d) {
-            getAll();
-        }, function (error) {
-            console.log('Oops! Something went wrong while saving the data.')
-        })
-    };
+    
   
     $scope.dltEquipo = function (id) {
         var dlt = Services.deleteEquipo(id);
